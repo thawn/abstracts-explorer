2001781af91099ddd076715c7a387090b3f7d6fc
Till Korten <webmaster@korten.at>
2026-04-01 22:01:50 +0200

    Apply suggestion from @thawn

--- tests/test_plugins_igarss.py
+++ tests/test_plugins_igarss.py
@@ -159,7 +159,7 @@ class TestIGARSSPluginMetadata:
         # _start_year is 2000 so years from 2000 onward should be included
         assert 2020 in plugin.supported_years
         assert 2024 in plugin.supported_years
-        assert 1999 not in plugin.supported_years
+        assert 1993 not in plugin.supported_years
 
     def test_get_metadata(self):
         plugin = IGARSSDownloaderPlugin()
